917247ec6a702f7a1d4943ab1f87264afc06f4e8
muuhab <mohab.barca@gmail.com>
2022-02-20 13:52:33 +0200

    Changed Navbar logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ export default function ButtonAppBar() {
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        User
+                        WhereMoney
                     </Typography>
                     <Button color="inherit">Logout</Button>
                 </Toolbar>
